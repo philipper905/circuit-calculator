7e91b9c1e4935ddf68c0fc40325224209bc8b2cb
Philippe R <philipper905@gmail.com>
2018-06-05 23:35:54 +0000

    Add compatibility for older versions of rust

--- src/circuits/circuit.rs
+++ src/circuits/circuit.rs
@@ -17,19 +17,19 @@ impl Circuit {
     fn print_helper(&self, s: &mut String, n: u32) -> u32 {
         let mut current_offset = n;
         match self {
-            Circuit::Resistor(Resistance(r), Voltage(v), Current(c)) => {
+            &Circuit::Resistor(Resistance(r), Voltage(v), Current(c)) => {
                 s.push_str(&format!(
                     "r{}.\tR: {:.3} Ohms,\tV: {:.3} Volts,\tI: {:.3} Amps\n",
                     n, r, v, c
                 ));
                 n + 1
             }
-            Circuit::Series(cir, _, _) => {
+            &Circuit::Series(ref cir, _, _) => {
                 cir.iter()
                     .for_each(|c| current_offset = c.print_helper(s, current_offset));
                 current_offset
             }
-            Circuit::Parallel(cir, _, _) => {
+            &Circuit::Parallel(ref cir, _, _) => {
                 cir.iter()
                     .for_each(|c| current_offset = c.print_helper(s, current_offset));
                 current_offset
--- src/error/mod.rs
+++ src/error/mod.rs
@@ -11,10 +11,10 @@ pub enum Error {
 impl ::std::error::Error for Error {
     fn description(&self) -> &str {
         match self {
-            Error::UnclosedParallel => "Forgot to close parallel circuit, expected an '>' and got an ']'",
-            Error::UnclosedSeries   => "Forgot to close series circuit, expected an ']' and got an '>'",
-            Error::UnclosedUnknown  => "Got to the end of parsing, and there was still a circuit unclosed, meaning either a ']' or a '>' was missing",
-            Error::UnopenedCircuit  => "Seems like the circuit was never opened"
+            &Error::UnclosedParallel => "Forgot to close parallel circuit, expected an '>' and got an ']'",
+            &Error::UnclosedSeries   => "Forgot to close series circuit, expected an ']' and got an '>'",
+            &Error::UnclosedUnknown  => "Got to the end of parsing, and there was still a circuit unclosed, meaning either a ']' or a '>' was missing",
+            &Error::UnopenedCircuit  => "Seems like the circuit was never opened"
         }
     }
 }
@@ -30,10 +30,10 @@ impl fmt::Display for Error {
             f,
             "{}",
             match self {
-                Error::UnclosedSeries => a,
-                Error::UnclosedParallel => b,
-                Error::UnclosedUnknown => c,
-                Error::UnopenedCircuit => d,
+                &Error::UnclosedSeries => a,
+                &Error::UnclosedParallel => b,
+                &Error::UnclosedUnknown => c,
+                &Error::UnopenedCircuit => d,
             }
         )
     }
